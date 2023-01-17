fa0bd1207462467194d6adf6dc700bf1d2464439
absalom12 <thomaslopezpagat@gmail.com>
2023-01-17 12:04:17 +0100

    final e2 TP1

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -6,12 +6,12 @@
 // REQUIRES the following Arduino libraries:
 // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
 // - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
-#include <Arduino.h>
+
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DHTPIN 2     // Digital pin connected to the DHT sensor 
+#define DHTPIN 33     // Digital pin connected to the DHT sensor 
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
@@ -30,8 +30,8 @@ uint32_t delayMS;
 void setup() {
   Serial.begin(9600);
   // Initialize device.
-  dht.begin();
   Serial.println(F("DHTxx Unified Sensor Example"));
+  dht.begin();
   // Print temperature sensor details.
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
@@ -54,7 +54,7 @@ void setup() {
   Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
   Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details
+  // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
 }
 
